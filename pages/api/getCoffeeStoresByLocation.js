bc985a58003cf13147812c3cafac5a4874e8bb44
Ayrow <aymeric.pilaert@hotmail.fr>
2022-07-22 10:37:35 +0200

    Set up serverless function for fetching coffee stores

---
+++ pages/api/getCoffeeStoresByLocation.js
@@ -0,0 +1,21 @@
+import { fetchCoffeeStores } from '../../lib/coffee-stores';
+
+const getCoffeeStoresByLocation = async (req, res) => {
+  //configure LatLong and limit
+  try {
+    const { latLong, limit } = req.query;
+
+    const response = await fetchCoffeeStores(latLong, limit);
+
+    res.status(200);
+    res.json(response);
+  } catch (error) {
+    console.log('there is an error', error);
+    res.status(500);
+    res.json({ message: 'Oh no! Something went wrong', error });
+  }
+
+  //return
+};
+
+export default getCoffeeStoresByLocation;
